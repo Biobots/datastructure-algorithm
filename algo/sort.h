ee5a10e4cb54d5fc07c7c7e1e889b5f446b5b1a0
biobot <biobot94@gmail.com>
2019-11-05 12:19:41 +0800

    quick sort based on stack
    Signed-off-by: biobot <biobot94@gmail.com>

--- algo/sort.h
+++ algo/sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <common.h>
+#include <stack.h>
 
 template <typename T>
 void quickSortProcess(T* data, int lo, int hi)
@@ -37,6 +38,44 @@ void quickSort(T* data, size_t size)
     size_t lo = 0;
     size_t hi = size - 1;
     quickSortProcess(data, lo, hi);
+}
+
+#include <tuple>
+template <typename T>
+void quickSortStack(T* data, size_t size)
+{
+    typedef std::tuple<int, int> U;
+    if (size <= 1) return;
+    SeqStack<U> stack(size);
+    stack.push(U(0, size - 1));
+    while (!stack.isEmpty())
+    {
+        U cur = stack.pop();
+        int lo = std::get<0>(cur);
+        int hi = std::get<1>(cur);
+        if (lo >= hi) continue;
+        T store = data[lo];
+        int lptr = lo;
+        int hptr = hi;
+        while (lptr < hptr)
+        {
+            while (data[hptr] >= store && hptr > lptr) hptr--;
+            if (hptr > lptr)
+            {
+                data[lptr] = data[hptr];
+                lptr++;
+            }
+            while (data[lptr] < store && hptr > lptr) lptr++;
+            if (hptr > lptr)
+            {
+                data[hptr] = data[lptr];
+                hptr--;
+            }
+        }
+        data[lptr] = store;
+        stack.push(U(lo, lptr - 1));
+        stack.push(U(hptr + 1, hi));
+    }
 }
 
 #endif
--- test/sorttest.cpp
+++ test/sorttest.cpp
@@ -16,4 +16,18 @@ TEST_CASE("sort test", "[Sort]")
             REQUIRE(arr[i] == i + 1);
         }
     }
+
+    SECTION("quick sort stack ver")
+    {
+        int arr[10];
+        for (int i = 0; i < 10; i++)
+        {
+            arr[i] = 10 - i;
+        }
+        quickSortStack(arr, 10);
+        for (int i = 0; i < 10; i++)
+        {
+            REQUIRE(arr[i] == i + 1);
+        }
+    }
 }
